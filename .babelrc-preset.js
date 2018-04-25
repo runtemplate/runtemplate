132fed3bd7e0bbd8af19b56bfde4b3c3a919aee6
Eric <ericff@gmail.com>
2018-04-25 13:50:47 +0800

    project setup

---
+++ .babelrc-preset.js
@@ -0,0 +1,31 @@
+module.exports = ({ env }) => {
+  const envStr = env && env()
+  return {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          es6: {
+            targets: { browsers: '> 1%' },
+            modules: false,
+          },
+          cjs: {
+            targets: { uglify: true },
+          },
+          test: { targets: { node: 'current' } },
+        }[envStr],
+      ],
+    ].filter(Boolean),
+
+    plugins: [
+      '@babel/plugin-proposal-object-rest-spread',
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-export-default-from',
+
+      'lodash',
+      // '@babel/plugin-proposal-optional-chaining',
+      '@babel/plugin-proposal-nullish-coalescing-operator',
+      '@babel/plugin-proposal-pipeline-operator',
+    ],
+  }
+}
